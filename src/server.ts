2d59231abc2bef40308c08b0421967ef00a8bb45
M4ster3001-Ubuntu <aldocfjunior@gmail.com>
2021-05-02 18:45:12 -0300

    finalizado o uploda e iniciando a documentação com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const PORT = 3333;
 
 console.clear();
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
