b361f50029a4a7efba8d292405e45215fffdbd20
M4ster3001-Ubuntu <aldocfjunior@gmail.com>
2021-10-24 18:39:00 -0300

    configurando o jest, adicionado os testes na criação e autenticação do usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,64 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepository as UsersLocalRepository } from "../../repositories/in-memory/UsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersLocalRepository: UsersLocalRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersLocalRepository = new UsersLocalRepository();
+    authenticateUserUseCase = new AuthenticateUserUseCase(usersLocalRepository);
+    createUserUseCase = new CreateUserUseCase(usersLocalRepository);
+  });
+
+  it("should be able to authenticate an use", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "000123",
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      username: "tester",
+      password: "1234",
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should not be able to authenticate an non-existent user", () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "teste@gmail.com",
+        password: "teste123",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate with incorrect password", () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_license: "98988889",
+        name: "John Doe2",
+        email: "johndoe2@gmail.com",
+        username: "tester2",
+        password: "312s12sdfsa23343",
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "teste123",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,36 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+import { CategoriesRepository as CategoriesLocalRepository } from "../../repositories/in-memory/CategoriesRepository";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategory: CreateCategoryUseCase;
+let categoriesLocalRepository: CategoriesLocalRepository;
+
+describe("Create category", () => {
+  beforeEach(() => {
+    categoriesLocalRepository = new CategoriesLocalRepository();
+    createCategory = new CreateCategoryUseCase(categoriesLocalRepository);
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = { name: "teste", description: "testando os testes" };
+
+    await createCategory.execute(category);
+
+    const categoryCreated = await categoriesLocalRepository.findByName(
+      category.name
+    );
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = { name: "teste", description: "testando os testes" };
+
+      await createCategory.execute(category);
+
+      await createCategory.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
